f2b37fc65f66021904aad841856e201210e8861d
bws9000 <snyder.burt@gmail.com>
2021-05-31 18:49:44 -0400

    ip update

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 import Client from "./js/Client.js";
 import VerizonDisplay from "./js/VerizonDisplay.js";
+
+let ip = '54.205.23.184';
+//let ip = 'localhost';
+
 const client = new Client(
-  new WebSocket("ws://localhost:3001/"),
+  new WebSocket("ws://" + ip + ":3001/"),
   new VerizonDisplay(document),
   false,
   false
